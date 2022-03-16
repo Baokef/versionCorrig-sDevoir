465ce9d5cdecf2d8a0901975fc790f215e86da6d
kefrn Bao <keprhen88@gmail.com>
2022-03-16 10:27:10 +0100

    new modif

--- just_prix_v3.py
+++ just_prix_v3.py
@@ -57,56 +57,56 @@ elif mode_choisi == 2:
             exit()
 elif mode_choisi == 3:
     print("bienvenu dans le juste prix !")
-Prix_max = int(input(f"choisi le prix max {''}"))
-nbr_essai = int(input(f"choisi le nombre d'essais {''}"))
-if nbr_essai == 0:
-    propostionDePrix = int(input("Veuillez entrer votre prix : "))
-    justPrix = randint(1, Prix_max)
-    while True:
-        if propostionDePrix < justPrix:
-            print("c'est plus ! \n")
-            propostionDePrix = int(input("Veuillez entrer votre prix : "))
+    Prix_max = int(input(f"choisi le prix max {''}"))
+    nbr_essai = int(input(f"choisi le nombre d'essais {''}"))
+    if nbr_essai == 0:
+        propostionDePrix = int(input("Veuillez entrer votre prix : "))
+        justPrix = randint(1, Prix_max)
+        while True:
+            if propostionDePrix < justPrix:
+                print("c'est plus ! \n")
+                propostionDePrix = int(input("Veuillez entrer votre prix : "))
 
-        elif propostionDePrix > justPrix:
-            print("c'est moins ! \n")
-            propostionDePrix = int(input("Veuillez entrer votre prix : "))
+            elif propostionDePrix > justPrix:
+                print("c'est moins ! \n")
+                propostionDePrix = int(input("Veuillez entrer votre prix : "))
 
-        elif propostionDePrix == justPrix:
-            print(justPrix)
-            print(
-                f"Félicitations, le juste prix était bien de {justPrix} ... ! \n")
+            elif propostionDePrix == justPrix:
+                print(justPrix)
+                print(
+                    f"Félicitations, le juste prix était bien de {justPrix} ... ! \n")
 
-            break
-        else:
-            exit()
-else:
-    nbr_essai = nbr_essai
-    print("bienvenu dans le juste prix !")
-    essai = nbr_essai
-    print(f"vous avez   {nbr_essai} essais ")
-    propostionDePrix = int(input("Veuillez entrer votre prix : "))
-    essai -= 1
-    justPrix = randint(1, Prix_max)
-    print(f"il vous reste   {essai} essais ")
-    while True and essai > 0:
-        if propostionDePrix < justPrix:
-            print("c'est plus ! \n")
-            propostionDePrix = int(input("Veuillez entrer votre prix : "))
-            essai -= 1
-            print(f"il vous reste   {essai} essais ")
+                break
+            else:
+                exit()
+    else:
+        nbr_essai = nbr_essai
+        print("bienvenu dans le juste prix !")
+        essai = nbr_essai
+        print(f"vous avez   {nbr_essai} essais ")
+        propostionDePrix = int(input("Veuillez entrer votre prix : "))
+        essai -= 1
+        justPrix = randint(1, Prix_max)
+        print(f"il vous reste   {essai} essais ")
+        while True and essai > 0:
+            if propostionDePrix < justPrix:
+                print("c'est plus ! \n")
+                propostionDePrix = int(input("Veuillez entrer votre prix : "))
+                essai -= 1
+                print(f"il vous reste   {essai} essais ")
 
-        elif propostionDePrix > justPrix:
-            print("c'est moins ! \n")
-            propostionDePrix = int(input("Veuillez entrer votre prix : "))
-            essai -= 1
-            print(f"il vous reste   {essai} essais ")
+            elif propostionDePrix > justPrix:
+                print("c'est moins ! \n")
+                propostionDePrix = int(input("Veuillez entrer votre prix : "))
+                essai -= 1
+                print(f"il vous reste   {essai} essais ")
 
-        elif propostionDePrix == justPrix:
-            print(justPrix)
-            print(
-                f"Félicitations, le juste prix était bien de {justPrix} ... ! \n")
+            elif propostionDePrix == justPrix:
+                print(justPrix)
+                print(
+                    f"Félicitations, le juste prix était bien de {justPrix} ... ! \n")
 
-            print(f"vous avez réussi en  {10-essai} essais ")
-            break
-        else:
-            exit()
+                print(f"vous avez réussi en  {10-essai} essais ")
+                break
+            else:
+                exit()
